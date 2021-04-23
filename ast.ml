176f1e92a188f2d82303b9d3fc56adff7673f265
Francesco Bertolaccini <francesco@bertolaccini.dev>
2021-04-23 17:48:50 +0200

    Add baseline blade implementation

--- ast.ml
+++ ast.ml
@@ -155,6 +155,34 @@ let print_cmd (c: cmd) : string =
 		| Auto -> "protect"
 		in Printf.sprintf "%s = %s(%s)" i prot_type (helper_rhs r)
 	in helper_cmd c
+
+type node_type =
+	| ExprN of expr
+	| RhsN of rhs
+	| VarN of string
+
+type 'n cmd_node =
+	| Skip
+	| Fail
+	| VarAssign of identifier * rhs * 'n * 'n
+	| PtrAssign of expr * expr
+	| ArrAssign of identifier * expr * expr
+	| Seq of 'n cmd_node * 'n cmd_node
+	| If of expr * 'n cmd_node * 'n cmd_node
+	| While of expr * 'n cmd_node
+	| Protect of identifier * protect * rhs
+
+let rec strip_nodes (c: 'n cmd_node) : (cmd) = match c with
+	| Skip -> Skip
+	| Fail -> Fail
+	| VarAssign (i, r, _, _) -> VarAssign(i, r)
+	| PtrAssign (e1, e2) -> PtrAssign (e1, e2)
+	| ArrAssign (i, e1, e2) -> ArrAssign (i, e1, e2)
+	| Seq (c1, c2) ->  Seq (strip_nodes c1, strip_nodes c2)
+	| If (e, c1, c2) -> If (e, strip_nodes c1, strip_nodes c2)
+	| While (e, c) -> While (e, strip_nodes c)
+	| Protect (i, p, r) -> Protect (i, p, r)
+
 open Graph
 open Flow_network
 
@@ -163,74 +191,109 @@ let rec blade c =
 	let module S = BFS(G) in
 	let module N = FlowNetworkMaker (G) (S) in
 	let var_nodes = Hashtbl.create 10 in
-	let create_node (r: rhs) (g: rhs G.graph) : (rhs G.graph * G.node) =
-		match Hashtbl.find_opt var_nodes r with
+	let create_node (n: node_type) (g: node_type G.graph) : (node_type G.graph * G.node) =
+		match Hashtbl.find_opt var_nodes n with
 		| Some node -> (g, node)
 		| None ->
-			let (g, node) = G.add_node g r in
-			Hashtbl.add var_nodes r node;
+			let (g, node) = G.add_node g n in
+			Hashtbl.add var_nodes n node;
 			(g, node)
-	in let rec blade_expr (e: expr) (g: rhs G.graph) (depth: int): (rhs G.graph * G.node) =
-		let (g, new_node) = create_node (Expr e) g in
+	in let rec defuse_expr (e: expr) (g: node_type G.graph) (depth: int): (node_type G.graph * G.node) =
+		let (g, new_node) = create_node (ExprN e) g in
 		match e with
 		| BinOp (e1, e2, op) ->
-			let (g, n1) = blade_expr e1 g depth in
-			let (g, n2) = blade_expr e2 g depth in
-			let g = G.connect g (n1, new_node) 1 in
-			let g = G.connect g (n2, new_node) 1 in (g, new_node)
+			let (g, n1) = defuse_expr e1 g depth in
+			let (g, n2) = defuse_expr e2 g depth in
+			let g = G.connect g (n1, new_node) max_int in
+			let g = G.connect g (n2, new_node) max_int in (g, new_node)
 		| InlineIf (e1, e2, e3) ->
-			let (g, n1) = blade_expr e1 g depth in
-			let (g, n2) = blade_expr e2 g depth in
-			let (g, n3) = blade_expr e3 g depth in
-			let g = G.connect g (n1, new_node) 1 in
-			let g = G.connect g (n2, new_node) 1 in
-			let g = G.connect g (n3, new_node) 1 in (g, new_node)
+			let (g, n1) = defuse_expr e1 g depth in
+			let (g, n2) = defuse_expr e2 g depth in
+			let (g, n3) = defuse_expr e3 g depth in
+			let g = G.connect g (n1, new_node) max_int in
+			let g = G.connect g (n2, new_node) max_int in
+			let g = G.connect g (n3, new_node) max_int in (g, new_node)
 		| _ -> (g, new_node)
-	in let blade_rhs (r: rhs) (g: rhs G.graph) (depth: int): (rhs G.graph * G.node) =
+	in let defuse_rhs (r: rhs) (g: node_type G.graph) (depth: int): (node_type G.graph * G.node) =
 		let source = G.source g in
 		let sink = G.sink g in
-		let (g, new_node) = create_node r g in
+		let (g, new_node) = create_node (RhsN r) g in
 		match r with
-		| Expr e -> blade_expr e g depth
+		| Expr e -> defuse_expr e g depth
 		| ArrayRead (i, e) ->
-			let (g, expr_node) = blade_expr e g depth in
-			let g = G.connect g (expr_node, sink) 1 in
-			let g = G.connect g (source, new_node) 1 in
-			let g = G.connect g (expr_node, new_node) 1 in (g, new_node)
+			let (g, expr_node) = defuse_expr e g depth in
+			let g = G.connect g (expr_node, sink) max_int in
+			let g = G.connect g (source, new_node) max_int in
+			let g = G.connect g (expr_node, new_node) max_int in (g, new_node)
 		| PtrRead e ->
-			let (g, expr_node) = blade_expr e g depth in
-			let g = G.connect g (expr_node, sink) 1 in
-			let g = G.connect g (source, new_node) 1 in
-			let g = G.connect g (expr_node, new_node) 1 in (g, new_node)
-	in let rec blade_cmd (c: cmd) (g: rhs G.graph) (depth: int): rhs G.graph =
+			let (g, expr_node) = defuse_expr e g depth in
+			let g = G.connect g (expr_node, sink) max_int in
+			let g = G.connect g (source, new_node) max_int in
+			let g = G.connect g (expr_node, new_node) max_int in (g, new_node)
+	in let rec defuse_cmd (c: cmd) (g: node_type G.graph) (depth: int): (node_type G.graph * G.node cmd_node) =
 		let sink = G.sink g in
 		match c with
-		| Skip | Fail -> g
 		| Seq (c1, c2) ->
-			let g = blade_cmd c1 g depth in
-			blade_cmd c2 g depth
+			let g, c1' = defuse_cmd c1 g depth in
+			let g, c2' = defuse_cmd c2 g depth in
+			(g, Seq(c1', c2'))
 		| If (e, c1, c2) ->
-			let (g, enode) = blade_expr e g depth in
-			let g = blade_cmd c1 g depth in
-			let g = blade_cmd c2 g depth in
-			G.connect g (enode, sink) 1
+			let g, enode = defuse_expr e g depth in
+			let g, c1' = defuse_cmd c1 g depth in
+			let g, c2' = defuse_cmd c2 g depth in
+			let g = G.connect g (enode, sink) max_int in
+			(g, If(e, c1', c2'))
 		| While (e, body) ->
-			let (g, enode) = blade_expr e g depth in
-			let g = blade_cmd body g (depth + 1) in
-			G.connect g (enode, sink) 1
+			let (g, enode) = defuse_expr e g depth in
+			let g, body' = defuse_cmd body g (depth + 1) in
+			let g = G.connect g (enode, sink) max_int in
+			(g, While (e, body'))
 		| VarAssign (name, r) ->
-			let (g, varnode) = create_node (Expr (Var name)) g in
-			let (g, rnode) = blade_rhs r g depth in
-			G.connect g (varnode, rnode) 1
+			let (g, varnode) = create_node (VarN name) g in
+			let (g, rnode) = defuse_rhs r g depth in
+			let g = G.connect g (varnode, rnode) 1 in
+			(g, VarAssign (name, r, varnode, rnode))
 		| PtrAssign (e1, e2) ->
-			let (g, n1) = blade_expr e1 g depth in
-			let (g, n2) = blade_expr e2 g depth in
-			G.connect g (n1, sink) 1
+			let (g, n1) = defuse_expr e1 g depth in
+			let (g, n2) = defuse_expr e2 g depth in
+			let g = G.connect g (n1, sink) max_int in
+			(g, PtrAssign (e1, e2))
 		| ArrAssign (i, e1, e2) ->
-			let (g, n1) = blade_expr e1 g depth in
-			let (g, n2) = blade_expr e2 g depth in
-			G.connect g (n1, sink) 1
+			let (g, n1) = defuse_expr e1 g depth in
+			let (g, n2) = defuse_expr e2 g depth in
+			let g = G.connect g (n1, sink) max_int in
+			(g, ArrAssign(i, e1, e2))
 		| Protect (name, prot, r) -> 
-			let (g, n) = blade_rhs r g depth in g
+			let (g, n) = defuse_rhs r g depth in
+			(g, Protect(name, prot, r))
+		| Skip -> (g, Skip)
+		| Fail -> (g, Fail)
+	in let defuse_graph, annotated_cmd = defuse_cmd c (G.empty ()) 0
+	in let min_cut = N.min_cut defuse_graph
+	in let rec repair_cmd (c: G.node cmd_node) : G.node cmd_node =
+		match c with
+		| Skip -> Skip
+		| Fail -> Fail
+		| VarAssign (i, r, ni, nr) ->
+			let ni_in_cut = List.exists (fun x -> x = ni) min_cut in
+			let nr_in_cut = List.exists (fun x -> x = nr) min_cut in
+			(match (ni_in_cut, nr_in_cut) with
+			| false, true -> Protect (i, Auto, r)
+			| true, false -> failwith "Invalid graph!"
+			| _ -> VarAssign(i, r, ni, nr))
+		| PtrAssign (e1, e2) -> PtrAssign (e1, e2)
+		| ArrAssign (i, e1, e2) -> ArrAssign (i, e1, e2)
+		| Seq (c1, c2) ->  Seq (repair_cmd c1, repair_cmd c2)
+		| If (e, c1, c2) -> If (e, repair_cmd c1, repair_cmd c2)
+		| While (e, c) -> While (e, repair_cmd c)
+		| Protect (i, p, r) -> Protect (i, p, r)
 
-	in failwith ""
+	in repair_cmd annotated_cmd |> strip_nodes
+
+let _ =
+	let c : cmd = (Seq(
+		VarAssign("x", ArrayRead("a", Var("i"))),
+		If(Var("x"), Skip, Skip))) in
+	Printf.printf "%s\n\n" (print_cmd c);
+	let c' = blade c in
+	Printf.printf "%s\n" (print_cmd c')
