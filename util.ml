44925a1a8305e9fb3ef5f324795b657c790abb95
Francesco Bertolaccini <francesco@bertolaccini.dev>
2021-04-26 09:33:10 +0200

    Fix print_expr

--- util.ml
+++ util.ml
@@ -1,5 +1,6 @@
 open Ast
 
+(** Returns a string representation of an expression. *)
 let rec print_expr (e: expr) : string = match e with
   | CstI i -> Printf.sprintf "%d" i
   | Var n -> n
@@ -11,18 +12,20 @@ let rec print_expr (e: expr) : string = match e with
   | Length i -> Printf.sprintf "length(%s)" i
   | Base i -> Printf.sprintf "base(%s)" i
 
+(** Returns a string representation of the right-hand side of an assignment. *)
 let print_rhs (r: rhs) : string = match r with
   | Expr e -> print_expr e
   | PtrRead e -> Printf.sprintf "*(%s)" (print_expr e)
   | ArrayRead (i, e) -> Printf.sprintf "%s[%s]" i (print_expr e)
 
+(** Returns a string representation of the right-hand side of a command. *)
 let print_cmd (c: cmd) : string =
   let rec helper_cmd (c: cmd) : string = match c with
     | Skip -> "skip"
     | Fail -> "fail"
     | VarAssign (i, r) -> Printf.sprintf "%s := %s" i (print_rhs r)
     | PtrAssign (e1, e2) -> Printf.sprintf "*(%s) := %s" (print_expr e1) (print_expr e2)
-    | ArrAssign (i, e1, e2) -> Printf.sprintf "%s(%s) := %s" i (print_expr e1) (print_expr e2)
+    | ArrAssign (i, e1, e2) -> Printf.sprintf "%s[%s] := %s" i (print_expr e1) (print_expr e2)
     | Seq (c1, c2) -> Printf.sprintf "%s;\n%s" (helper_cmd c1) (helper_cmd c2)
     | If (e, c1, c2) -> Printf.sprintf "if %s then\n%s\nelse\n%s\nendif" (print_expr e) (helper_cmd c1) (helper_cmd c2)
     | While (e, c) -> Printf.sprintf "while %s do\n%s\nendwhile" (print_expr e) (helper_cmd c)
