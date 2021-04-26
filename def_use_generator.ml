80f99d958584a79d3bd9897054b85eb3645b650f
Francesco Bertolaccini <francesco@bertolaccini.dev>
2021-04-26 09:18:23 +0200

    Add different protect cost models

--- def_use_generator.ml
+++ def_use_generator.ml
@@ -32,6 +32,11 @@ let rec strip_nodes (c: 'n cmd_node) : cmd =
   | WhileN (e, c) -> While (e, strip_nodes c)
   | ProtectN (i, p, r) -> Protect (i, p, r)
 
+(** Interface for functions that estimate the cost of assignments *)
+module type CostEstimator = sig
+  val get_cost: int -> rhs -> int
+end
+
 (** A DefUseGenerator generates the def-use graph of a syntactical element *)
 module type DefUseGenerator = sig
   type t
@@ -48,17 +53,17 @@ module type DefUseGenerator = sig
   val create_node: t -> node_type -> node
 
   (** Populates the def-use graph of an expression *)
-  val defuse_expr: t -> expr -> int -> node
+  val defuse_expr: t -> expr -> node
 
   (** Populates the def-use graph of the right-hand side of an assignment *)
-  val defuse_rhs: t -> rhs -> int -> node
+  val defuse_rhs: t -> rhs -> node
 
   (** Populates the def-use graph of a command *)
   val defuse_cmd: t -> cmd -> int -> node cmd_node
 end
 
 (** Generates a def-use graph using an Hashtbl implementation *)
-module HashtblGenerator (G : Graph)
+module HashtblGenerator (G : Graph) (E: CostEstimator)
   : DefUseGenerator
     with type node = G.node
      and type graph = node_type G.graph = struct
@@ -66,7 +71,7 @@ module HashtblGenerator (G : Graph)
   type node = G.node
   type t = {
     tbl: (node_type, node) Hashtbl.t;
-    g: graph; }
+    mutable g: graph; }
 
   let create = { tbl = Hashtbl.create 10; g = G.empty () }
 
@@ -78,29 +83,32 @@ module HashtblGenerator (G : Graph)
     | None ->
       let (g, node) = G.add_node gen.g n in
       Hashtbl.add gen.tbl n node;
+      gen.g <- g;
       node
 
-  let rec defuse_expr (gen: t) (e: expr) (depth: int): node =
+  let rec defuse_expr (gen: t) (e: expr): node =
     let ntype = match e with
       |Var name -> VarN name
       | _ -> ExprN e
     in let new_node = create_node gen ntype in
     match e with
     | BinOp (e1, e2, op) ->
-      let n1 = defuse_expr gen e1 depth in
-      let n2 = defuse_expr gen e2 depth in
-      let _ = G.connect gen.g (n1, new_node) max_int in
-      let _ = G.connect gen.g (n2, new_node) max_int in new_node
+      let n1 = defuse_expr gen e1 in
+      let n2 = defuse_expr gen e2 in
+      gen.g <- G.connect gen.g (n1, new_node) max_int;
+      gen.g <- G.connect gen.g (n2, new_node) max_int;
+      new_node
     | InlineIf (e1, e2, e3) ->
-      let n1 = defuse_expr gen e1 depth in
-      let n2 = defuse_expr gen e2 depth in
-      let n3 = defuse_expr gen e3 depth in
-      let _ = G.connect gen.g (n1, new_node) max_int in
-      let _ = G.connect gen.g (n2, new_node) max_int in
-      let _ = G.connect gen.g (n3, new_node) max_int in new_node
+      let n1 = defuse_expr gen e1 in
+      let n2 = defuse_expr gen e2 in
+      let n3 = defuse_expr gen e3 in
+      gen.g <- G.connect gen.g (n1, new_node) max_int;
+      gen.g <- G.connect gen.g (n2, new_node) max_int;
+      gen.g <- G.connect gen.g (n3, new_node) max_int;
+      new_node
     | _ -> new_node
 
-  let defuse_rhs (gen: t) (r: rhs) (depth: int): node =
+  let defuse_rhs (gen: t) (r: rhs): node =
     let source = G.source gen.g in
     let sink = G.sink gen.g in
     let ntype = match r with
@@ -108,17 +116,17 @@ module HashtblGenerator (G : Graph)
       | _ -> RhsN r
     in let new_node = create_node gen ntype in
     match r with
-    | Expr e -> defuse_expr gen e depth
+    | Expr e -> defuse_expr gen e
     | ArrayRead (i, e) ->
-      let expr_node = defuse_expr gen e depth in
-      let _ = G.connect gen.g (expr_node, sink) max_int in
-      let _ = G.connect gen.g (source, new_node) max_int in
+      let expr_node = defuse_expr gen e in
+      gen.g <- G.connect gen.g (expr_node, sink) max_int;
+      gen.g <- G.connect gen.g (source, new_node) max_int;
       new_node
     | PtrRead e ->
-      let expr_node = defuse_expr gen e depth in
-      let _ = G.connect gen.g (expr_node, sink) max_int in
-      let _ = G.connect gen.g (source, new_node) max_int in
-      let _ = G.connect gen.g (expr_node, new_node) max_int in
+      let expr_node = defuse_expr gen e in
+      gen.g <- G.connect gen.g (expr_node, sink) max_int;
+      gen.g <- G.connect gen.g (source, new_node) max_int;
+      gen.g <- G.connect gen.g (expr_node, new_node) max_int;
       new_node
 
   let rec defuse_cmd (gen: t) (c: cmd) (depth: int): node cmd_node =
@@ -129,33 +137,34 @@ module HashtblGenerator (G : Graph)
       let c2' = defuse_cmd gen c2 depth in
       SeqN(c1', c2')
     | If (e, c1, c2) ->
-      let enode = defuse_expr gen e depth in
+      let enode = defuse_expr gen e in
       let c1' = defuse_cmd gen c1 depth in
       let c2' = defuse_cmd gen c2 depth in
-      let _ = G.connect gen.g (enode, sink) max_int in
+      gen.g <- G.connect gen.g (enode, sink) max_int;
       IfN(e, c1', c2')
     | While (e, body) ->
-      let enode = defuse_expr gen e depth in
+      let enode = defuse_expr gen e in
       let body' = defuse_cmd gen body (depth + 1) in
-      let _ = G.connect gen.g (enode, sink) max_int in
+      gen.g <- G.connect gen.g (enode, sink) max_int;
       WhileN (e, body')
     | VarAssign (name, r) ->
       let varnode = create_node gen (VarN name) in
-      let rnode = defuse_rhs gen r depth in
-      let _ = G.connect gen.g (rnode, varnode) 1 in
+      let rnode = defuse_rhs gen r in
+      let cost = E.get_cost depth r in
+      gen.g <- G.connect gen.g (rnode, varnode) cost;
       VarAssignN (name, r, varnode, rnode)
     | PtrAssign (e1, e2) ->
-      let n1 = defuse_expr gen e1 depth in
-      let _ = defuse_expr gen e2 depth in
-      let _ = G.connect gen.g (n1, sink) max_int in
+      let n1 = defuse_expr gen e1 in
+      let _ = defuse_expr gen e2 in
+      gen.g <- G.connect gen.g (n1, sink) max_int;
       PtrAssignN (e1, e2)
     | ArrAssign (i, e1, e2) ->
-      let n1 = defuse_expr gen e1 depth in
-      let _ = defuse_expr gen e2 depth in
-      let _ = G.connect gen.g (n1, sink) max_int in
+      let n1 = defuse_expr gen e1 in
+      let _ = defuse_expr gen e2 in
+      gen.g <- G.connect gen.g (n1, sink) max_int;
       ArrAssignN(i, e1, e2)
     | Protect (name, prot, r) ->
-      let _ = defuse_rhs gen r depth in
+      let _ = defuse_rhs gen r in
       ProtectN(name, prot, r)
     | Skip -> SkipN
     | Fail -> FailN
--- main.ml
+++ main.ml
@@ -9,10 +9,30 @@ open Graph
 open Flow_network
 open Def_use_generator
 
+let uniform_cost (d: int) (r: rhs): int = 1
+let exp_cost (d: int) (r: rhs): int = Int.shift_left 1 d
+let costly_fence (f: int -> rhs -> int) (d: int) (r: rhs): int =
+  let base_cost = f d r in
+  match r with
+  | ArrayRead(_, _) -> base_cost
+  | _ -> base_cost * 2
+
+module UniformCost : CostEstimator = struct
+  let get_cost = uniform_cost
+end
+
+module ExponentialCost : CostEstimator = struct
+  let get_cost = exp_cost
+end
+
+module ExponentialCostNoFences (E: CostEstimator) : CostEstimator = struct
+  let get_cost = costly_fence E.get_cost
+end
+
 module G = MatrixGraph
 module S = BFS (G)
 module N = FlowNetworkMaker (G) (S)
-module Gen = HashtblGenerator (G)
+module Gen = HashtblGenerator (G) (ExponentialCost)
 module B = BaselineBlade (G) (S) (N) (Gen)
 
 let _ =
